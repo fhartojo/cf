24bb56396f58e9bd7256dfc2a16214036d91c6af
Francis Hartojo <fhartojo@gmail.com>
2019-11-08 13:51:25 -0700

    Initial commit.

---
+++ src/main/java/com/quiteharmless/cf/CloudFoundryPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.quiteharmless.cf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudFoundryPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudFoundryPracticeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/quiteharmless/cf/CloudFoundryPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quiteharmless.cf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudFoundryPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
